987c176e3ca02ed74c68ba563c6e2ef8805e5590
Jesus Tordesillas <jtorde@mit.edu>
2020-04-23 14:31:22 -0400

    put params in a class

--- faster/scripts/dynamic_obstacles.py
+++ faster/scripts/dynamic_obstacles.py
@@ -16,6 +16,7 @@ from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from geometry_msgs.msg import Point
 
+from std_msgs.msg import ColorRGBA
 
 import numpy as np
 from numpy import linalg as LA
@@ -29,6 +30,37 @@ import tf
 from math import sin, cos, tan
 
 
+import copy 
+
+
+color_static=ColorRGBA(r=0,g=0,b=1,a=1);
+color_dynamic=ColorRGBA(r=1,g=0,b=0,a=1);
+
+class MovingCircle:
+    def __init__(self):
+        self.radius=1.0;
+        self.num_of_objects=3;
+        self.scale=1.0;
+        self.slower_min=1
+        self.slower_max= 1.5
+
+class MovingCorridor:
+    def __init__(self):
+        self.num_of_dyn_objects=0;
+        self.num_of_stat_objects=0;
+        self.x_min= 0.0
+        self.x_max= 50.0
+        self.y_min= -1.0 
+        self.y_max= +1.0
+        self.z_min= 0.0 
+        self.z_max= 1.0
+        self.scale=1.0;
+        self.slower_min=1
+        self.slower_max= 1.5
+        self.bbox_dynamic=[0.6, 0.6, 0.6]
+        self.bbox_static=[0.4, 0.4, 1.2]
+
+
 
 class FakeSim:
 
@@ -39,15 +71,14 @@ class FakeSim:
         name = rospy.get_namespace()
         self.name = name[1:-1]
 
-        self.pubTraj = rospy.Publisher('/trajs', DynTraj, queue_size=20, latch=True)
+       #self.num_of_objects = 0;
 
-        self.pubShapes = rospy.Publisher('/shapes', Marker, queue_size=1, latch=True)
+        world_type="MovingCorridor"
 
-        self.timer = rospy.Timer(rospy.Duration(0.001), self.pubTF)
-
-        self.pubGazeboState = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
-        self.num_of_objects = 0;
-
+        if(world_type=="MovingCorridor"):
+            self.world=MovingCorridor()
+        if(world_type=="MovingCircle"):
+            self.world=MovingCircle()
    
 
         self.x_all=[];
@@ -55,57 +86,74 @@ class FakeSim:
         self.z_all=[];
         self.offset_all=[];
         self.slower=[];
-        for i in range(self.num_of_objects):
-            self.x_all.append(50*random.random());
-            self.y_all.append(self.x_all[i]+2*random.random());
+        self.type=[];#"dynamic" or "static"
+        for i in range(self.world.num_of_dyn_objects):          
+            self.x_all.append(random.uniform(self.world.x_min, self.world.x_max));
+            self.y_all.append(random.uniform(self.world.y_min, self.world.y_max));
             self.z_all.append(1);
             self.offset_all.append(random.uniform(-2*math.pi, 2*math.pi));
-            self.slower.append(random.uniform(1, 1.5));
-            # self.x_all.append(random.random());
-            # self.y_all.append(4*random.random());
-            # self.z_all.append(2);
-            # self.x_all.append(50*random.random());
-            # self.y_all.append(1*random.random());
-            # self.z_all.append(2);
+            self.slower.append(random.uniform(self.world.slower_min, self.world.slower_max));
+            self.type.append("dynamic")
 
-        # self.state.quat.x = 0
-        # self.state.quat.y = 0
-        # self.state.quat.z = 0
-        # self.state.quat.w = 1
+        for i in range(self.world.num_of_stat_objects):          
+            self.x_all.append(random.uniform(self.world.x_min-self.world.scale, self.world.x_max+self.world.scale));
+            self.y_all.append(random.uniform(self.world.y_min-self.world.scale, self.world.y_max+self.world.scale));
+            self.z_all.append(random.uniform(self.world.z_min-self.world.scale, self.world.z_max+self.world.scale));
+            self.offset_all.append(random.uniform(-2*math.pi, 2*math.pi));
+            self.slower.append(random.uniform(self.world.slower_min, self.world.slower_max));
+            self.type.append("static")
 
-        # self.pubGazeboState = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
+        self.pubTraj = rospy.Publisher('/trajs', DynTraj, queue_size=20, latch=True)
+        self.pubShapes_static = rospy.Publisher('/shapes_static', Marker, queue_size=1, latch=True)
+        self.pubShapes_dynamic = rospy.Publisher('/shapes_dynamic', Marker, queue_size=1, latch=True)
+        self.timer = rospy.Timer(rospy.Duration(0.001), self.pubTF)
+        self.pubGazeboState = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
 
         rospy.sleep(0.5)
 
-        # self.state.header.frame_id="world"
-        # self.pubState.publish(self.state)  
 
     def pubTF(self, timer):
         br = tf.TransformBroadcaster()
 
-        marker=Marker()
-        marker.header.frame_id="world"
-        marker.type=marker.CUBE_LIST;
-        marker.action=marker.ADD;
-        marker.color.r=0
-        marker.color.g=0
-        marker.color.b=1
-        marker.color.a=1
+        marker_tmp=Marker();
+        marker_tmp.header.frame_id="world"
+        marker_tmp.type=marker_tmp.CUBE_LIST;
+        marker_tmp.action=marker_tmp.ADD;
 
-        for i in range(self.num_of_objects):
+        marker_static=copy.deepcopy(marker_tmp);
+        marker_dynamic=copy.deepcopy(marker_tmp);
+
+        marker_dynamic.color=color_dynamic;
+        marker_dynamic.scale.x=self.world.bbox_dynamic[0]
+        marker_dynamic.scale.y=self.world.bbox_dynamic[1]
+        marker_dynamic.scale.z=self.world.bbox_dynamic[2]
+
+        marker_static.color=color_static;
+        marker_static.scale.x=self.world.bbox_static[0]
+        marker_static.scale.y=self.world.bbox_static[1]
+        marker_static.scale.z=self.world.bbox_static[2]
+
+        for i in range(self.world.num_of_dyn_objects + self.world.num_of_stat_objects):
             t_ros=rospy.Time.now()
             t=rospy.get_time(); #Same as before, but it's float
-            [x_string, y_string, z_string] = self.trefoil(self.x_all[i], self.y_all[i], self.z_all[i], 1, 1, 1, self.offset_all[i], self.slower[i]) 
+
+            dynamic_trajectory_msg=DynTraj(); 
+
+            if(self.type[i]=="dynamic"):
+              s=self.world.scale;
+              [x_string, y_string, z_string] = self.trefoil(self.x_all[i], self.y_all[i], self.z_all[i], s,s,s, self.offset_all[i], self.slower[i]) 
+              dynamic_trajectory_msg.bbox = self.world.bbox_dynamic;
+            else:
+              [x_string, y_string, z_string] = self.static(self.x_all[i], self.y_all[i], self.z_all[i]);
+              dynamic_trajectory_msg.bbox = self.world.bbox_static;
+
             x = eval(x_string)
             y = eval(y_string)
             z = eval(z_string)
-            dynamic_trajectory_msg=DynTraj(); 
 
             dynamic_trajectory_msg.is_agent=False;
-
             dynamic_trajectory_msg.header.stamp= t_ros;
             dynamic_trajectory_msg.function = [x_string, y_string, z_string]
-            dynamic_trajectory_msg.bbox = [0.6, 0.6, 0.6]
             dynamic_trajectory_msg.pos.x=x #Current position
             dynamic_trajectory_msg.pos.y=y #Current position
             dynamic_trajectory_msg.pos.z=z #Current position
@@ -125,20 +173,23 @@ class FakeSim:
             # gazebo_state.reference_frame = "world" 
             # self.pubGazeboState.publish(gazebo_state)  
 
-            #If you want to see the objets in rviz
-            marker.scale.x=dynamic_trajectory_msg.bbox[0]
-            marker.scale.y=dynamic_trajectory_msg.bbox[1]
-            marker.scale.z=dynamic_trajectory_msg.bbox[2]
-
+            #If you want to see the objects in rviz
             point=Point()
             point.x=x;
             point.y=y;
             point.z=z;
-            marker.points.append(point);
+            if(self.type[i]=="dynamic"):
+                marker_dynamic.points.append(point);
+            if(self.type[i]=="static"):
+                marker_static.points.append(point);
 
-        self.pubShapes.publish(marker)
+        self.pubShapes_static.publish(marker_static)
+        self.pubShapes_dynamic.publish(marker_dynamic)
 
 
+
+    def static(self,x,y,z):
+        return [str(x), str(y), str(z)]
 
     # Trefoil knot, https://en.wikipedia.org/wiki/Trefoil_knot
     def trefoil(self,x,y,z,scale_x, scale_y, scale_z, offset, slower):
@@ -146,9 +197,9 @@ class FakeSim:
         #slower=1.0; #The higher, the slower the obstacles move" 
         tt='t/' + str(slower)+'+';
 
-        x_string='(sin('+tt +str(offset)+') + 2 * sin(2 * '+tt +str(offset)+'))/' + str(scale_x) +'+' + str(x); #'2*sin(t)' 
-        y_string='(cos('+tt +str(offset)+') - 2 * cos(2 * '+tt +str(offset)+'))/' + str(scale_y) +'+' + str(y); #'2*cos(t)' 
-        z_string='-sin(3 * '+tt +str(offset)+')/' +str(scale_z) + '+' + str(z);                               #'1.0'        
+        x_string=str(scale_x)+'*(sin('+tt +str(offset)+') + 2 * sin(2 * '+tt +str(offset)+'))' +'+' + str(x); #'2*sin(t)' 
+        y_string=str(scale_y)+'*(cos('+tt +str(offset)+') - 2 * cos(2 * '+tt +str(offset)+'))' +'+' + str(y); #'2*cos(t)' 
+        z_string=str(scale_z)+'*(-sin(3 * '+tt +str(offset)+'))' + '+' + str(z);                               #'1.0'        
 
         # x_string='sin('+tt +str(offset)+')';
         # y_string='cos('+tt +str(offset)+')';
@@ -168,15 +219,25 @@ if __name__ == '__main__':
 
     ns = rospy.get_namespace()
     try:
-        # rospy.init_node('relay')
-        # if str(ns) == '/':
-        #     rospy.logfatal("Need to specify namespace as vehicle name.")
-        #     rospy.logfatal("This is tyipcally accomplished in a launch file.")
-        #     rospy.logfatal("Command line: ROS_NAMESPACE=mQ01 $ rosrun quad_control joy.py")
-        # else:
-        #     print "Starting joystick teleop node for: " + ns
-        #     startNode()
         rospy.init_node('dynamic_obstacles')
         startNode()
     except rospy.ROSInterruptException:
         pass
+
+
+            # self.x_all.append(random.random());
+            # self.y_all.append(4*random.random());
+            # self.z_all.append(2);
+            # self.x_all.append(50*random.random());
+            # self.y_all.append(1*random.random());
+            # self.z_all.append(2);
+
+        # self.state.quat.x = 0
+        # self.state.quat.y = 0
+        # self.state.quat.z = 0
+        # self.state.quat.w = 1
+
+        # self.pubGazeboState = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
+
+        # self.state.header.frame_id="world"
+        # self.pubState.publish(self.state)  
