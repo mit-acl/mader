95001f72ec0a8e76c1513316c82edbbb59eea590
Jesus Tordesillas <jtorde@mit.edu>
2020-09-09 14:14:37 -0400

    minor

--- install_and_compile.sh
+++ install_and_compile.sh
@@ -2,6 +2,7 @@
 # Author: Jesus Tordesillas Torres
 
 source ~/.bashrc
+sudo apt-get update
 
 #INSTALL NLOPT v2.6.2
 ##########################################
@@ -24,7 +25,6 @@ sudo make install
 ##########################################
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get update
 sudo apt-get install python-catkin-tools -y
 
 #CLONE SUBMODULES, INSTALL DEPENDENCIES AND COMPILE
