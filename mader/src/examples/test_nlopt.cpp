e1d351c412f6b13c06c761cd94117a75b0ebff62
Jesus Tordesillas <jtorde@mit.edu>
2020-11-09 15:52:53 -0500

    small example for nlopt

--- mader/src/examples/test_nlopt.cpp
+++ mader/src/examples/test_nlopt.cpp
@@ -7,108 +7,106 @@
 
 #include <Eigen/Dense>
 #include <random>
-#include "./../../timer.hpp"
+#include "timer.hpp"
 
-#include "solverNlopt.hpp"
+#include "solver_nlopt.hpp"
 
 #include <fstream>
 
 typedef MADER_timers::Timer MyTimer;
 
-int main()
+ConvexHullsOfCurve createStaticObstacle(double x, double y, double z, int num_pol, double bbox_x, double bbox_y,
+                                        double bbox_z)
 {
-  double z_ground = -2.0;
-  double z_max = 2.0;
-  double dc = 0.01;
-  double Ra = 4.0;
-  int deg = 3;
-  int samples_per_interval = 1;
-  double weight = 10000.0;
-  double epsilon_tol_constraints = 0.1;
-  double xtol_rel = 1e-07;
-  double ftol_rel = 1e-07;
-  std::string solver = "LD_MMA";
-  double kappa = 0.0;
-  double mu = 1.0;
-  int a_star_samp_x = 7;
-  int a_star_samp_y = 7;
-  int a_star_samp_z = 7;
+  ConvexHullsOfCurve hulls_curve;
+  std::vector<Point_3> points;
 
-  double increment = 0.3;  // grid used to prune nodes that are on the same cell
+  points.push_back(Point_3(x - bbox_x / 2.0, y - bbox_y / 2.0, z - bbox_z / 2.0));
+  points.push_back(Point_3(x - bbox_x / 2.0, y - bbox_y / 2.0, z + bbox_z / 2.0));
+  points.push_back(Point_3(x - bbox_x / 2.0, y + bbox_y / 2.0, z + bbox_z / 2.0));
+  points.push_back(Point_3(x - bbox_x / 2.0, y + bbox_y / 2.0, z - bbox_z / 2.0));
 
-  double runtime = 1.0;  //(not use this criterion)  //[seconds]
+  points.push_back(Point_3(x + bbox_x / 2.0, y + bbox_y / 2.0, z + bbox_z / 2.0));
+  points.push_back(Point_3(x + bbox_x / 2.0, y + bbox_y / 2.0, z - bbox_z / 2.0));
+  points.push_back(Point_3(x + bbox_x / 2.0, y - bbox_y / 2.0, z + bbox_z / 2.0));
+  points.push_back(Point_3(x + bbox_x / 2.0, y - bbox_y / 2.0, z - bbox_z / 2.0));
 
-  Eigen::Vector3d v_max(10.0, 10.0, 10.0);
-  Eigen::Vector3d a_max(60.0, 60.0, 60.0);
+  CGAL_Polyhedron_3 hull_interval = convexHullOfPoints(points);
 
-  state initial;
-  initial.pos = Eigen::Vector3d(-4.0, 0.0, 0.0);
-
-  state final;
-  final.pos = Eigen::Vector3d(4.0, 0.0, 0.0);
-
-  double t_min = 0.0;
-  double t_max = t_min + (final.pos - initial.pos).norm() / (0.3 * v_max(0));
-
-  Polyhedron_Std hull;
-
-  hull.push_back(Eigen::Vector3d(-0.5, -0.5, -70.0));
-
-  hull.push_back(Eigen::Vector3d(-0.5, 0.5, 70.0));
-  hull.push_back(Eigen::Vector3d(0.5, -0.5, 70.0));
-  hull.push_back(Eigen::Vector3d(0.5, 0.5, -70.0));
-
-  hull.push_back(Eigen::Vector3d(-0.5, -0.5, 70.0));
-  hull.push_back(Eigen::Vector3d(0.5, -0.5, -70.0));
-  hull.push_back(Eigen::Vector3d(-0.5, 0.5, -70.0));
-
-  hull.push_back(Eigen::Vector3d(0.5, 0.5, 70.0));
-
-  std::ofstream myfile;
-  myfile.open("/home/jtorde/Desktop/ws/src/mader/mader/src/solvers/nlopt/example.txt");
-
-  double tmp = 8.0;
-  //  for (double tmp = 3; tmp < 50; tmp = tmp + 0.05)
-  // {
-  int n_pol = ceil(tmp);
-  std::cout << "**************************************************" << std::endl;
-  std::cout << "**************************************************" << std::endl;
-  std::cout << "TRYING WITH n_pol= " << n_pol << std::endl;
-
-  ConvexHullsOfCurves_Std hulls_curves;
-  ConvexHullsOfCurve_Std hulls_curve;
-  // Assummes static obstacle
-  for (int i = 0; i < n_pol; i++)
+  for (int i = 0; i < num_pol; i++)
   {
-    hulls_curve.push_back(hull);
+    hulls_curve.push_back(hull_interval);  // static obstacle
   }
 
-  hulls_curves.push_back(hulls_curve);
+  return hulls_curve;
+}
 
-  /////
-  SolverNlopt snlopt(n_pol, deg, hulls_curves.size(), weight, epsilon_tol_constraints, xtol_rel, ftol_rel,
-                     solver);  // snlopt(a,g) a polynomials of degree 3
-  snlopt.setBasisUsedForCollision(snlopt.B_SPLINE);
-  snlopt.setHulls(hulls_curves);
-  snlopt.setDistanceToUseStraightLine(Ra / 2.0);
-  snlopt.setKappaAndMu(kappa, mu);
-  snlopt.setZminZmax(z_ground, z_max);
-  snlopt.setAStarSamplesAndFractionVoxel(a_star_samp_x, a_star_samp_y, a_star_samp_z, 0.5);
-  snlopt.setMaxValues(v_max.x(), a_max.x());  // v_max and a_max
-  snlopt.setDC(dc);                           // dc
-  snlopt.setTminAndTmax(t_min, t_max);
-  snlopt.setMaxRuntime(runtime);
-  snlopt.setInitAndFinalStates(initial, final);
+int main()
+{
+  double bbox_x = 0.4;
+  double bbox_y = 0.4;
+  double bbox_z = 0.4;
+  int num_pol = 4;
+
+  ConvexHullsOfCurve hulls_curve = createStaticObstacle(0.0, 0.0, bbox_z / 2.0, num_pol, bbox_x, bbox_y, bbox_z);
+  ConvexHullsOfCurves hulls_curves;
+  hulls_curves.push_back(hulls_curve);
+  ConvexHullsOfCurves_Std hulls_std = vectorGCALPol2vectorStdEigen(hulls_curves);
+
+  par_snlopt parameters;
+  parameters.v_max = 20 * Eigen::Vector3d::Ones();
+  parameters.a_max = 20 * Eigen::Vector3d::Ones();
+  parameters.dc = 0.01;
+  parameters.dist_to_use_straight_guess = 1;
+  parameters.a_star_samp_x = 5;
+  parameters.a_star_samp_y = 5;
+  parameters.a_star_samp_z = 5;
+  parameters.a_star_fraction_voxel_size = 0.5;
+  parameters.num_pol = num_pol;
+  parameters.deg_pol = 3;
+  parameters.weight = 1.0;
+  parameters.epsilon_tol_constraints = 0.001;
+  parameters.xtol_rel = 0.0000000000001;
+  parameters.ftol_rel = 0.0000000000001;
+  parameters.solver = "LD_MMA";
+  parameters.basis = "MINVO";
+  parameters.a_star_bias = 1.0;
+  parameters.allow_infeasible_guess = true;
+  parameters.Ra = 4.0;
+
+  parameters.alpha_shrink = 0.95;
+
+  SolverNlopt snlopt(parameters);  // snlopt(a,g) a polynomials of degree 3
+  snlopt.setMaxRuntimeKappaAndMu(0.2, 0.5, 0.5);
+  state initial_state;
+  initial_state.pos = Eigen::Vector3d(-4.0, 0.0, 0.0);
+
+  state final_state;
+  final_state.pos = Eigen::Vector3d(4.0, 0.0, 0.0);
+
+  double t_min = 0.0;
+  double t_max = t_min + (final_state.pos - initial_state.pos).norm() / (0.3 * parameters.v_max(0));
+
+  snlopt.setInitStateFinalStateInitTFinalT(initial_state, final_state, t_min, t_max);
+
+  snlopt.setHulls(hulls_std);
 
   std::cout << "Calling optimize" << std::endl;
   bool converged = snlopt.optimize();
 
   double time_needed = snlopt.getTimeNeeded();
-  double delta = (t_max - t_min) / n_pol;
-  if (converged)
-  {
-    myfile << n_pol << ", " << delta << ", " << time_needed << std::endl;
-  }
-  //  }
-  myfile.close();
+  double delta = (t_max - t_min) / num_pol;
 }
+// std::ofstream myfile;
+// myfile.open("/home/jtorde/Desktop/ws/src/mader/mader/src/solvers/nlopt/example.txt");
+
+// double tmp = 8.0;
+//  for (double tmp = 3; tmp < 50; tmp = tmp + 0.05)
+// {
+
+// if (converged)
+// {
+//   myfile << num_pol << ", " << delta << ", " << time_needed << std::endl;
+// }
+// //  }
+// myfile.close();
