9d504aff7ce380e57cfc0d38e0237ef90a1135dc
Kota Kondo <kkondo@mit.edu>
2022-06-02 10:55:18 -0400

    collision detector update

--- mader/scripts/collision_detector.py
+++ mader/scripts/collision_detector.py
@@ -21,7 +21,7 @@ class CollisionDetector:
 
         self.initialized = False
 
-        self.world_pos = np.empty([6,3])
+        self.state_pos = np.empty([6,3])
 
     # collision detection
     def collisionDetect(self, timer):
@@ -29,36 +29,36 @@ class CollisionDetector:
         if self.initialized:
             for i in range(5):
                 for j in range(i+1,6):
-                    if (abs(self.world_pos[i,0] - self.world_pos[j,0]) < self.bbox_x 
-                        and abs(self.world_pos[i,1] - self.world_pos[j,1]) < self.bbox_y 
-                        and abs(self.world_pos[i,2] - self.world_pos[j,2]) < self.bbox_z):
-                        print("difference in x is " + str(abs(self.world_pos[i,0] - self.world_pos[j,0])))
-                        print("difference in y is " + str(abs(self.world_pos[i,1] - self.world_pos[j,1])))
-                        print("difference in z is " + str(abs(self.world_pos[i,2] - self.world_pos[j,2])))
+                    if (abs(self.state_pos[i,0] - self.state_pos[j,0]) < self.bbox_x 
+                        and abs(self.state_pos[i,1] - self.state_pos[j,1]) < self.bbox_y 
+                        and abs(self.state_pos[i,2] - self.state_pos[j,2]) < self.bbox_z):
+                        print("difference in x is " + str(abs(self.state_pos[i,0] - self.state_pos[j,0])))
+                        print("difference in y is " + str(abs(self.state_pos[i,1] - self.state_pos[j,1])))
+                        print("difference in z is " + str(abs(self.state_pos[i,2] - self.state_pos[j,2])))
                         print("agent" + str(i+1) + " and " + str(j+1) + " collide")
 
-    def SQ01worldCB(self, data):
-        self.world_pos[0,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ01stateCB(self, data):
+        self.state_pos[0,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
         self.initialized = True
-    def SQ02worldCB(self, data):
-        self.world_pos[1,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
-    def SQ03worldCB(self, data):
-        self.world_pos[2,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
-    def SQ04worldCB(self, data):
-        self.world_pos[3,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
-    def SQ05worldCB(self, data):
-        self.world_pos[4,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
-    def SQ06worldCB(self, data):
-        self.world_pos[5,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ02stateCB(self, data):
+        self.state_pos[1,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ03stateCB(self, data):
+        self.state_pos[2,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ04stateCB(self, data):
+        self.state_pos[3,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ05stateCB(self, data):
+        self.state_pos[4,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ06stateCB(self, data):
+        self.state_pos[5,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
 
 def startNode():
     c = CollisionDetector()
-    rospy.Subscriber("SQ01s/world", PoseStamped, c.SQ01worldCB)
-    rospy.Subscriber("SQ02s/world", PoseStamped, c.SQ02worldCB)
-    rospy.Subscriber("SQ03s/world", PoseStamped, c.SQ03worldCB)
-    rospy.Subscriber("SQ04s/world", PoseStamped, c.SQ04worldCB)
-    rospy.Subscriber("SQ05s/world", PoseStamped, c.SQ05worldCB)
-    rospy.Subscriber("SQ06s/world", PoseStamped, c.SQ06worldCB)
+    rospy.Subscriber("SQ01s/state", PoseStamped, c.SQ01stateCB)
+    rospy.Subscriber("SQ02s/state", PoseStamped, c.SQ02stateCB)
+    rospy.Subscriber("SQ03s/state", PoseStamped, c.SQ03stateCB)
+    rospy.Subscriber("SQ04s/state", PoseStamped, c.SQ04stateCB)
+    rospy.Subscriber("SQ05s/state", PoseStamped, c.SQ05stateCB)
+    rospy.Subscriber("SQ06s/state", PoseStamped, c.SQ06stateCB)
     rospy.Timer(rospy.Duration(0.01), c.collisionDetect)
     rospy.spin()
 
