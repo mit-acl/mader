c3b6bb2ae088212d47a560768540557d51a8c86c
Kota Kondo <kkondo@mit.edu>
2022-06-24 15:24:04 -0400

    minor

--- mader/scripts/run_many_sims_multi_agent.py
+++ mader/scripts/run_many_sims_multi_agent.py
@@ -19,6 +19,7 @@ import subprocess
 import numpy as np
 import time
 from numpy import linalg as LA
+import struct
 
 def getGoals(num_of_agents):
 
@@ -116,14 +117,17 @@ def checkGoalReached(num_of_agents):
     for i in range(1, num_of_agents+1):
         pos = np.empty(3)
         if i <= 9:
-            pos[0] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/x', '-n', '1']))
-            pos[1] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/y', '-n', '1']))
-            pos[2] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/z', '-n', '1']))
+            # print(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/x', '-n', '1']))
+            # string = subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/x', '-n', '1'])
+            # print(string.decode())
+            pos[0] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/x', '-n', '1']).decode()[:-6])
+            pos[1] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/y', '-n', '1']).decode()[:-6])
+            pos[2] = float(subprocess.check_output(['rostopic', 'echo', '/SQ0' + str(i) + 's/state/pos/z', '-n', '1']).decode()[:-6])
             print(pos)
         else:
-            pos[0] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/x', '-n', '1']))
-            pos[1] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/y', '-n', '1']))
-            pos[2] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/z', '-n', '1']))
+            pos[0] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/x', '-n', '1']).decode()[:-6])
+            pos[1] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/y', '-n', '1']).decode()[:-6])
+            pos[2] = float(subprocess.check_output(['rostopic', 'echo', '/SQ' + str(i) + 's/state/pos/z', '-n', '1']).decode()[:-6])
         
         goal_radius = 0.15 # set by mader.yaml
 
