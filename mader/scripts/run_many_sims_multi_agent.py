9b341c08badc79584994019980021edcba6dae04
Kota Kondo <kkondo@mit.edu>
2022-06-24 15:43:09 -0400

    minor

--- mader/scripts/run_many_sims_multi_agent.py
+++ mader/scripts/run_many_sims_multi_agent.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
         tic = time.perf_counter()
         toc = time.perf_counter()
 
-        while (toc - tic < 120 or not is_goal_reached):
+        while (toc - tic < 60 and not is_goal_reached):
             toc = time.perf_counter()
             if(checkGoalReached(num_of_agents)):
                 print('all the agents reached the goal')
