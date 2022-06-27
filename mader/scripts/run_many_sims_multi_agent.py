4f966dc164676b1c72ae00674a4e9c791fd607fb
Kota Kondo <kkondo@mit.edu>
2022-06-27 10:20:52 -0400

    delay check 0.0s

--- mader/scripts/run_many_sims_multi_agent.py
+++ mader/scripts/run_many_sims_multi_agent.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
     commands = []
 
     # other strings
-    folder_bags="/home/kota/data/bags/rmader";
+    folder_bags="/home/kota/data/bags/oldmader";
     # name_node_record="bag_recorder"
     kill_all="tmux kill-server & killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient & killall -9 roscore & killall -9 rosmaster & pkill mader_node & pkill -f dynamic_obstacles & pkill -f rosout & pkill -f behavior_selector_node & pkill -f rviz & pkill -f rqt_gui & pkill -f perfect_tracker & pkill -f mader_commands"
 
