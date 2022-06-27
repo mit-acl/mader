1c9728109ffad2ed68d14261cf222c9b4ad8c3aa
Kota Kondo <kkondo@mit.edu>
2022-06-27 09:57:38 -0400

    collision_check.py

--- mader/other/sim/collision_check.py
+++ mader/other/sim/collision_check.py
@@ -45,6 +45,8 @@ if __name__ == '__main__':
         
         log_data = b.message_by_topic("/is_collided")
         if (log_data == None):
+        	print("sim " + sim_id + ": no collision" )
         	os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')
        	else:
+        	print("sim " + sim_id + ": ******collision******" )
         	os.system('echo "simulation '+sim_id+': ******collision******" >> '+source_dir+'/collision_status.txt')
