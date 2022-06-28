5389d76b332faaa40ef55c693cd1b7588e1ae6d3
Kota Kondo <kkondo@mit.edu>
2022-06-28 15:36:48 -0400

    minor

--- mader/other/sim/comm_delay_histogram_percentile.py
+++ mader/other/sim/comm_delay_histogram_percentile.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     # Dont use ~ like this
     cd = "50" # [ms] communication delay
-    dc = "100" # [ms] delay check
+    dc = "150" # [ms] delay check
     figname = 'cd_'+cd+'_dc_'+dc+'_rmader_comm_delay_histogram.png'
     source_dir = "/home/kota/data/bags" # change the source dir accordingly #10 agents 
     # source_dir = "/home/kota/data/bags/multi_agent/sim_num_1_2022-06-24-20-48-34_bag_comm_delay_proof" # change the source dir accordingly #10 agents 
