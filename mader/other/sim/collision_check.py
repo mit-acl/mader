a8e94d7b06fb4a041d6900c304b19c5586735dff
Kota Kondo <kkondo@mit.edu>
2022-06-28 17:13:17 -0400

    minor

--- mader/other/sim/collision_check.py
+++ mader/other/sim/collision_check.py
@@ -49,11 +49,11 @@ if __name__ == '__main__':
         if (log_data == None):
         	print("sim " + sim_id + ": no collision" )
         	# os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')
-            os.system("sed -n '"+i+"s/$/  [[:space:]] no collision' >> "+source_dir+"/status.txt")
+            os.system("sed -n '"+str(i)+"s/$/  [[:space:]] no collision' >> "+source_dir+"/status.txt")
        	else:
         	print("sim " + sim_id + ": ******collision******" )
         	# os.system('echo "simulation '+sim_id+': ******collision******" > '+source_dir+'/collision_status.txt')
-            os.system("sed -n '"+i+"s/$/  [[:space:]] no collision' >> "+source_dir+"/status.txt")
+            os.system("sed -n '"+str(i)+"s/$/  [[:space:]] no collision' >> "+source_dir+"/status.txt")
 
 
 
