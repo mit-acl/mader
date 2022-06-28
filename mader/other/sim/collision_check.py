0b8c84bc19efe722cbdc20e6bef0185b41d22e49
Kota Kondo <kkondo@mit.edu>
2022-06-28 16:45:42 -0400

    minor

--- mader/other/sim/collision_check.py
+++ mader/other/sim/collision_check.py
@@ -28,9 +28,10 @@ if __name__ == '__main__':
     # Dont use ~ like this
     cd = "50" # [ms] communication delay
     dc = "150" # [ms] delay check
-    source_dir = "/home/kota/data/bags" # change the source dir accordingly #10 agents 
+    source_dir = "/home/kota/data/bags/cd_"+cd+"ms_dc_"+dc+"ms/rmader" # change the source dir accordingly #10 agents
+    source_len = len(source_dir)
     # source_dir = "/home/kota/data/bags/multi_agent/sim_num_1_2022-06-24-20-48-34_bag_comm_delay_proof" # change the source dir accordingly #10 agents 
-    source_bags = source_dir + "/cd_"+cd+"ms_dc_"+dc+"ms/rmader/*.bag" # change the source dir accordingly
+    source_bags = source_dir + "/*.bag" # change the source dir accordingly
 
     rosbag_list = glob.glob(source_bags)
     rosbag_list.sort() #alphabetically order
