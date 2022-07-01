246ed6bb49ae1f38f5c191f12f818b774ec57b9d
Kota Kondo <kkondo@mit.edu>
2022-07-01 12:28:30 -0400

    comm_delay_histogram percentile update

--- mader/other/sim/comm_delay_histogram_percentile.py
+++ mader/other/sim/comm_delay_histogram_percentile.py
@@ -2,8 +2,8 @@
 # Kota Kondo
 
 #  Commands: Open three terminals and execute this:
-#  python comm_delay_histogram_percentile.py input_comm_delay
-#  Ex. python comm_delay_histogram_percentile.py 0.150
+#  python comm_delay_histogram_percentile.py
+#  Ex. python comm_delay_histogram_percentile.py
 
 # change cd and dc
 
@@ -27,58 +27,70 @@ import numpy
 
 if __name__ == '__main__':
 
-    # Dont use ~ like this
     cd = "50" # [ms] communication delay
-    dc = "250" # [ms] delay check
-    figname = 'cd_'+cd+'_dc_'+dc+'_rmader_comm_delay_histogram.png'
-    source_dir = "/home/kota/data/bags" # change the source dir accordingly #10 agents 
-    # source_dir = "/home/kota/data/bags/multi_agent/sim_num_1_2022-06-24-20-48-34_bag_comm_delay_proof" # change the source dir accordingly #10 agents 
-    # source_bags = source_dir + "/rmader/cd_"+cd+"ms_dc_"+dc+"ms/*.bag" # change the source dir accordingly
-    source_bags = source_dir + "/oldmader/cd_"+cd+"ms/*.bag" # change the source dir accordingly
 
-    rosbag_list = glob.glob(source_bags)
-    rosbag_list.sort() #alphabetically order
-    rosbag = []
-    comm_delay = []
+    dc_list = [0, 250, 87, 78, 63, 55] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+
+    # you wanna get histogram or know the value at q-th percentile
+    is_histogram = True
+    # q-th percentile
+    q = 75
+
+    is_oldmader = True
     num_of_agents = 10
 
-    # comm_delay you use
-    input_comm_delay = float(sys.argv[1])
+    for dc in dc_list:
 
-    for bag in rosbag_list:
-        rosbag.append(bag)
+        figname = 'cd_'+cd+'_dc_'+str(dc)+'_rmader_comm_delay_histogram.png'
+        source_dir = "/home/kota/data/bags" # change the source dir accordingly #10 agents 
+        if is_oldmader:
+            source_bags = source_dir + "/oldmader/cd"+cd+"ms/*.bag" # change the source dir accordingly #10 agents
+        else:
+            source_bags = source_dir + "/rmader/cd"+cd+"msdc"+str(dc)+"ms/*.bag" # change the source dir accordingly #10 agents
 
-    for i in range(len(rosbag)):
+        rosbag_list = glob.glob(source_bags)
+        rosbag_list.sort() #alphabetically order
+        rosbag = []
+        comm_delay = []
 
-        b = bagreader(rosbag[i], verbose=False);
-        
-        for i in range(1,num_of_agents+1):
-            if i < 10:
-                log_data = b.message_by_topic("/SQ0" + str(i) + "s/mader/comm_delay")
-            else:
-                log_data = b.message_by_topic("/SQ" + str(i) + "s/mader/comm_delay")
+        # comm_delay you use
+        input_comm_delay = float(sys.argv[1])
 
-            log = pd.read_csv(log_data)
+        for bag in rosbag_list:
+            rosbag.append(bag)
 
-            for j in range(len(log.comm_delay)):
-                comm_delay.append(log.comm_delay[j])
+        for i in range(len(rosbag)):
 
-    # print percentile
-    comm_delay_arr = numpy.array(comm_delay)
-    # percentile = scipy.stats.percentileofscore(comm_delay_arr, input_comm_delay, kind='mean')
-    # os.system('echo "cd='+cd+', dc='+dc+':   '+str(input_comm_delay) + ' is ' + str(percentile) + '-th percentile" >> '+source_dir+'/comm_delay_percentile.txt')
-    
-    # in case you wanna calculate the value of q-th percentile
-    q = 75
-    print(str(q) + "-th percentile value is " + str(numpy.percentile(comm_delay_arr, q)))
+            b = bagreader(rosbag[i], verbose=False);
+            
+            for i in range(1,num_of_agents+1):
+                if i < 10:
+                    log_data = b.message_by_topic("/SQ0" + str(i) + "s/mader/comm_delay")
+                else:
+                    log_data = b.message_by_topic("/SQ" + str(i) + "s/mader/comm_delay")
 
-    # # print(comm_delay)
-    # max_comm_delay = max(comm_delay)
+                log = pd.read_csv(log_data)
 
-    # n, bins, patches = plt.hist(x=comm_delay)
-    # plt.grid(axis='y', color='black', alpha=0.2)
-    # plt.title('Comm delay histogram \n max comm_delay is '+str(round(max_comm_delay,3))+' [s]')
-    # plt.xlabel("comm delay [s]")
-    # plt.ylabel("count")
-    # plt.savefig('data/'+figname)
-    # # plt.show()
+                for j in range(len(log.comm_delay)):
+                    comm_delay.append(log.comm_delay[j])
+
+        # print percentile
+        comm_delay_arr = numpy.array(comm_delay)
+
+        if is_histogram:
+            percentile = scipy.stats.percentileofscore(comm_delay_arr, input_comm_delay, kind='mean')
+            os.system('echo "cd='+cd+', dc='+str(dc)+':   '+str(input_comm_delay) + ' is ' + str(percentile) + '-th percentile" >> '+source_dir+'/comm_delay_percentile.txt')
+            # print(comm_delay)
+            max_comm_delay = max(comm_delay)
+
+            n, bins, patches = plt.hist(x=comm_delay)
+            plt.rcParams["font.family"] = "Times New Roman"
+            plt.grid(axis='y', color='black', alpha=0.2)
+            plt.title('Comm delay histogram \n max comm_delay is '+str(round(max_comm_delay,3))+' [s]')
+            plt.xlabel("comm delay [s]")
+            plt.ylabel("count")
+            plt.savefig('data/'+figname)
+            # plt.show()
+        else:
+            # in case you wanna calculate the value of q-th percentile
+            print(str(q) + "-th percentile value is " + str(numpy.percentile(comm_delay_arr, q)))
