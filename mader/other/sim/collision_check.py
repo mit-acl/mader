efabbee0761847cf7dc86ea910b91da2a81e94de
kotakondo <kkondo@mit.edu>
2022-07-28 13:36:44 -0400

    collision_check.py improved

--- mader/other/sim/collision_check.py
+++ mader/other/sim/collision_check.py
@@ -26,45 +26,51 @@ if __name__ == '__main__':
     # rosbag name
 
     # Dont use ~ like this
-    cd = "50" # [ms] communication delay
 
     is_oldmader = True # change here 
     
     if is_oldmader:
-        dc_list = [0, 160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        cd_list = [0, 50, 100, 150, 200, 300, 400, 500]
+        # dc_list = [0, 160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        dc_list = [0] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
     else:
+        cd_list = [50, 100, 150, 200, 300, 400, 500]
         dc_list = [160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
 
-    for dc in dc_list:
+    for cd in cd_list:
 
-        if is_oldmader:
-            source_dir = "/home/kota/data/bags/oldmader/cd"+cd+"ms" # change the source dir accordingly #10 agents
-        else:
-            source_dir = "/home/kota/data/bags/rmader/cd"+cd+"msdc"+str(dc)+"ms" # change the source dir accordingly #10 agents
-        
-        source_len = len(source_dir)
-        source_bags = source_dir + "/*.bag" # change the source dir accordingly
+        is_oldmader=True
 
-        rosbag_list = glob.glob(source_bags)
-        rosbag_list.sort() #alphabetically order
-        rosbag = []
+        for dc in dc_list:
 
-        for bag in rosbag_list:
-            rosbag.append(bag)
-
-        for i in range(len(rosbag)):
-
-            b = bagreader(rosbag[i], verbose=False)
-            sim_id = rosbag[i][source_len+5:source_len+7]
-            
-            log_data = b.message_by_topic("/is_collided")
-            if (log_data == None):
-                print("sim " + sim_id + ": no collision" )
-                os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')
+            if is_oldmader:
+                source_dir = "/home/kota/data/bags/oldmader/cd"+str(cd)+"ms" # change the source dir accordingly #10 agents
             else:
-                print("sim " + sim_id + ": ******collision******" )
-                os.system('echo "simulation '+sim_id+': ***collision***" >> '+source_dir+'/collision_status.txt')
+                source_dir = "/home/kota/data/bags/rmader/cd"+str(cd)+"msdc"+str(dc)+"ms" # change the source dir accordingly #10 agents
+            
+            source_len = len(source_dir)
+            source_bags = source_dir + "/*.bag" # change the source dir accordingly
 
-        os.system('paste '+source_dir+'/collision_status.txt '+source_dir+'/status.txt >> '+source_dir+'/complete_status.txt')
+            rosbag_list = glob.glob(source_bags)
+            rosbag_list.sort() #alphabetically order
+            rosbag = []
 
-        is_oldmader = False
+            for bag in rosbag_list:
+                rosbag.append(bag)
+
+            for i in range(len(rosbag)):
+
+                b = bagreader(rosbag[i], verbose=False)
+                sim_id = rosbag[i][source_len+5:source_len+7]
+                
+                log_data = b.message_by_topic("/is_collided")
+                if (log_data == None):
+                    print("sim " + sim_id + ": no collision" )
+                    os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')
+                else:
+                    print("sim " + sim_id + ": ******collision******" )
+                    os.system('echo "simulation '+sim_id+': ***collision***" >> '+source_dir+'/collision_status.txt')
+
+            os.system('paste '+source_dir+'/collision_status.txt '+source_dir+'/status.txt >> '+source_dir+'/complete_status.txt')
+
+            is_oldmader = False
