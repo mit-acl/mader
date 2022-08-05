ac692173db1cb0c56648f5c0eebb39574be31414
kotakondo <kkondo@mit.edu>
2022-08-05 18:31:57 +0000

    ave distance csv 2 txt update

--- mader/other/sim/ave_distance_csv2txt.py
+++ mader/other/sim/ave_distance_csv2txt.py
@@ -32,71 +32,96 @@ if __name__ == '__main__':
     num_of_agents = 10 
     
     if is_oldmader:
-        dc_list = [0, 160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        cd_list = [50, 100, 200, 300]
     else:
-        dc_list = [160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        cd_list = [50, 100]
 
-    for dc in dc_list:
+    for cd in cd_list:
 
-        dist_matrix_dc = np.zeros([num_of_agents, num_of_agents])
+        is_oldmader=True
 
-        if is_oldmader:
-            source_dir = "/home/kota/data/csv/oldmader/cd"+cd+"ms" # change the source dir accordingly #10 agents
+        if cd == 50:
+            dc_list = [0, 160, 55, 51, 50.5, 50.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+            # dc_list = [0, 160] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        elif cd == 100:
+            dc_list = [0, 210, 105, 101, 100.5, 100.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+            # dc_list = [0, 210] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
         else:
-            source_dir = "/home/kota/data/csv/rmader/cd"+cd+"msdc"+str(dc)+"ms" # change the source dir accordingly #10 agents
-        
-        source_len = len(source_dir)
-        source_csvs = source_dir + "/*.csv" # change the source dir accordingly
+            dc_list =[0]
 
-        csv_list = glob.glob(source_csvs)
-        csv_list.sort() #alphabetically order
-        csv = []
+        for dc in dc_list:
 
-        for csv_each in csv_list:
-            csv.append(csv_each)
+            dc_in_ms = dc/1000;
+            cd_in_ms = cd/1000;
 
-        # print(csv)
+            if dc == 50.5:
+                str_dc = "50_5"
+            elif dc == 50.1:
+                str_dc = "50_1"
+            elif dc == 100.5:
+                str_dc = "100_5"
+            elif dc == 100.1:
+                str_dc = "100_1"
 
-        num_of_sims = len(csv)
+            dist_matrix_dc = np.zeros([num_of_agents, num_of_agents])
 
-        # going through the csvs (sims)
-        for i in range(len(csv)):
-            dist_matrix = pd.read_csv(csv[i])
-            dist_matrix = dist_matrix.to_numpy()
-            dist_matrix = dist_matrix[:,1:]
-
-            # print(dist_matrix)
-
-            # going through the agents
-            dist_matrix_dc = np.add(dist_matrix_dc, dist_matrix)
-
-        # averageing over num of sim
-        num_of_sims_matrix = num_of_sims * np.ones((num_of_agents,num_of_agents))
-        # print(num_of_sims_matrix)
-        dist_matrix_dc = dist_matrix_dc / num_of_sims_matrix
-        # print(dist_matrix_dc)
-
-        # create symmetric matrix
-        for i in range(num_of_agents):
-            for j in range(i+1, num_of_agents):
-                dist_matrix_dc[j,i] = dist_matrix_dc[i,j]
-
-        # print(dist_matrix_dc)
-
-        sum_of_rows = dist_matrix_dc.sum(axis=1)
-        # print(sum_of_rows)
-        ave_per_dc = sum_of_rows / ((num_of_agents-1)*np.ones(num_of_agents)) #num_of_agents-1 is becasue you don't calcuate the distance from itself to itself 
-        # print(ave_per_dc)
-
-        os.system('echo "'+source_dir+'" >> '+source_dir+'/ave_distance.txt')
-        for i in range(num_of_agents):
-            if i<=9:
-                sim_id = "0"+str(i)
+            if is_oldmader:
+                source_dir = "/home/kota/data/csv/oldmader/cd"+cd+"ms" # change the source dir accordingly #10 agents
             else:
-                sim_id = str(i)
-            os.system('echo "SQ'+sim_id+' '+str(ave_per_dc[i])+'" >> '+source_dir+'/ave_distance.txt')
+                source_dir = "/home/kota/data/csv/rmader/cd"+cd+"ms/dc"+str_dc+"ms" # change the source dir accordingly #10 agents
+            
+            source_len = len(source_dir)
+            source_csvs = source_dir + "/*.csv" # change the source dir accordingly
 
-        total_ave_dist = sum(ave_per_dc)/len(ave_per_dc)
-        os.system('echo "total distance '+str(total_ave_dist)+'" >> '+source_dir+'/ave_distance.txt')
+            csv_list = glob.glob(source_csvs)
+            csv_list.sort() #alphabetically order
+            csv = []
 
-        is_oldmader = False
+            for csv_each in csv_list:
+                csv.append(csv_each)
+
+            # print(csv)
+
+            num_of_sims = len(csv)
+
+            # going through the csvs (sims)
+            for i in range(len(csv)):
+                dist_matrix = pd.read_csv(csv[i])
+                dist_matrix = dist_matrix.to_numpy()
+                dist_matrix = dist_matrix[:,1:]
+
+                # print(dist_matrix)
+
+                # going through the agents
+                dist_matrix_dc = np.add(dist_matrix_dc, dist_matrix)
+
+            # averageing over num of sim
+            num_of_sims_matrix = num_of_sims * np.ones((num_of_agents,num_of_agents))
+            # print(num_of_sims_matrix)
+            dist_matrix_dc = dist_matrix_dc / num_of_sims_matrix
+            # print(dist_matrix_dc)
+
+            # create symmetric matrix
+            for i in range(num_of_agents):
+                for j in range(i+1, num_of_agents):
+                    dist_matrix_dc[j,i] = dist_matrix_dc[i,j]
+
+            # print(dist_matrix_dc)
+
+            sum_of_rows = dist_matrix_dc.sum(axis=1)
+            # print(sum_of_rows)
+            ave_per_dc = sum_of_rows / ((num_of_agents-1)*np.ones(num_of_agents)) #num_of_agents-1 is becasue you don't calcuate the distance from itself to itself 
+            # print(ave_per_dc)
+
+            os.system('echo "'+source_dir+'" >> '+source_dir+'/ave_distance.txt')
+            for i in range(num_of_agents):
+                if i<=9:
+                    sim_id = "0"+str(i)
+                else:
+                    sim_id = str(i)
+                os.system('echo "SQ'+sim_id+' '+str(ave_per_dc[i])+'" >> '+source_dir+'/ave_distance.txt')
+
+            total_ave_dist = sum(ave_per_dc)/len(ave_per_dc)
+            os.system('echo "total distance '+str(total_ave_dist)+'" >> '+source_dir+'/ave_distance.txt')
+
+            is_oldmader = False
