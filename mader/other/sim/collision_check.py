697dd31dc5e6ef4ba87e20195e4b8bdaacc6c538
kotakondo <kkondo@mit.edu>
2022-08-05 18:32:18 +0000

    collision_check.py update

--- mader/other/sim/collision_check.py
+++ mader/other/sim/collision_check.py
@@ -25,46 +25,43 @@ if __name__ == '__main__':
 
     # rosbag name
 
-    # Dont use ~ like this
+    is_oldmader = True
 
-    is_oldmader = True # change here 
-    
     if is_oldmader:
-        cd_list = [0, 50, 100]
-        # dc_list = [0, 160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+        cd_list = [50, 100, 200, 300]
     else:
-        # cd_list = [50, 100, 150, 200, 300, 400, 500]
         cd_list = [50, 100]
-        dc_list = [160, 120, 100, 78, 63, 55, 51] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
 
     for cd in cd_list:
 
         is_oldmader=True
 
-        if cd == 50:
-            # dc_list = [160, 100, 60, 55, 51, 50.5, 50.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
-            dc_list = [0] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+       if cd == 50:
+            dc_list = [0, 160, 55, 51, 50.5, 50.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+            # dc_list = [0, 160] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
         elif cd == 100:
-            # dc_list = [230, 210, 150, 110, 105, 101, 100.5, 100.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
-            dc_list = [0] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+            dc_list = [0, 210, 105, 101, 100.5, 100.1] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
+            # dc_list = [0, 210] #dc_list[0] will be used for old mader (which doesn't need delay check) so enter some value (default 0)
         else:
             dc_list =[0]
-            
+
         for dc in dc_list:
 
+            collision_cnt = 0
+
             if dc == 50.5:
-                dc = 50_5
+                str_dc = "50_5"
             elif dc == 50.1:
-                dc = 50_1
+                str_dc = "50_1"
             elif dc == 100.5:
-                dc = 100_5
+                str_dc = "100_5"
             elif dc == 100.1:
-                dc = 100_1
+                str_dc = "100_1"
 
             if is_oldmader:
                 source_dir = "/home/kota/data/bags/oldmader/cd"+str(cd)+"ms" # change the source dir accordingly #10 agents
             else:
-                source_dir = "/home/kota/data/bags/rmader/cd"+str(cd)+"msdc"+str(dc)+"ms" # change the source dir accordingly #10 agents
+                source_dir = "/home/kota/data/bags/rmader/cd"+str(cd)+"ms/dc"+str_dc+"ms" # change the source dir accordingly #10 agents
             
             source_len = len(source_dir)
             source_bags = source_dir + "/*.bag" # change the source dir accordingly
@@ -86,9 +83,12 @@ if __name__ == '__main__':
                     print("sim " + sim_id + ": no collision" )
                     os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')
                 else:
+                    collision_cnt = collision_cnt + 1
                     print("sim " + sim_id + ": ******collision******" )
                     os.system('echo "simulation '+sim_id+': ***collision***" >> '+source_dir+'/collision_status.txt')
 
+            collision_per = collision_cnt / len(rosbag)
             os.system('paste '+source_dir+'/collision_status.txt '+source_dir+'/status.txt >> '+source_dir+'/complete_status.txt')
+            os.system('echo "'+source_dir+': '+str(collision_cnt)+' collisions/'+str(len(rosbag))+' - '+str(collision_per)+'%" >> home/kota/data/collision_count.txt')
 
             is_oldmader = False
