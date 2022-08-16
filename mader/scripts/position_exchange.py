49c8fae661e0daf278608dac40138e5ad3db8ca2
aclswarm <aerospacecontrolslabmit@gmail.com>
2022-08-16 19:09:26 -0400

    new param

--- mader/scripts/position_exchange.py
+++ mader/scripts/position_exchange.py
@@ -221,7 +221,7 @@ class TermGoalSender:
                     self.term_goal.pose.position.x = self.wp8[0]
                     self.term_goal.pose.position.y = self.wp8[1]
 
-            self.term_goal.pose.position.z = 1.0 + 0.9 * random()
+            self.term_goal.pose.position.z = 1.1 + 0.9 * random()
             # self.term_goal.pose.position.z = 1.0
 
             self.if_arrived = not self.if_arrived
