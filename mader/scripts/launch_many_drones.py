0ea66ade39abebf0be377c87f5151f55ac68d68b
kotakondo <kkondo@mit.edu>
2022-09-14 17:59:34 -0400

    100 agents

--- mader/scripts/launch_many_drones.py
+++ mader/scripts/launch_many_drones.py
@@ -56,8 +56,8 @@ if __name__ == '__main__':
     # formation="sphere", "square" "circle"
     formation="circle"
     commands = []
-    num_of_agents=50; 
-    radius=30;
+    num_of_agents=100; 
+    radius=40;
 
 
     if(formation=="sphere"):
